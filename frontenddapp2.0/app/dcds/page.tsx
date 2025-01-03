6ba6f3cb603149748d4f23b29bff9301a3ddc033
Hitesh-Gurnani <newidhiteshgurnani@gmail.com>
2025-01-03 17:48:58 +0530

    feat/added-dcds-component

--- frontenddapp2.0/app/dcds/page.tsx
+++ frontenddapp2.0/app/dcds/page.tsx
@@ -1,6 +1,188 @@
 import React from "react";
+import { WithdrawModal } from "../components/dcds/WithdrawModal";
+import Image from "next/image";
+import tokenImage from "../assets/Vector (6).png";
+import { Button } from "@/components/ui/button";
+import add from "../assets/add-01.png";
+import dcdsFrame from "../assets/Frame 350.png";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDownIcon } from "lucide-react";
+
+function TokenTvlDetails() {
+  return (
+    <div className="bg-gradient-to-b from-[#E5F3FF] to-[#E5F3FF] p-8 flex justify-between border border-solid border-grayLight border-b-0">
+      <div className="flex flex-col gap-8">
+        <Image src={tokenImage} alt="token" width={32} height={32} />
+        <span className="text-[24px] text-textBlack">USDc</span>
+      </div>
+      <div className="flex flex-col gap-8">
+        <span className="text-[18px] font-normal text-right text-grayLight">
+          TVL
+        </span>
+        <span className="text-[24px] font-medium text-textBlack">
+          $100,000,000
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function AdditionalDCDSMetrics() {
+  return (
+    <div className="p-5 flex flex-col gap-3">
+      <div className="flex justify-between">
+        <span className="text-grayLight text-[18px] font-medium ofnt-plex-grotesk">
+          APY
+        </span>
+        <span className="text-grayLight text-[18px] font-medium ofnt-plex-grotesk">
+          --
+        </span>
+      </div>
+      <div className="flex justify-between">
+        <span className="text-grayLight text-[18px] font-medium ofnt-plex-grotesk">
+          Depositing
+        </span>
+        <span className="text-grayLight text-[18px] font-medium ofnt-plex-grotesk">
+          --
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function SelectToken() {
+  return (
+    <div className="flex flex-col mt-4">
+      <Label
+        htmlFor="network"
+        className="font-plex-grotesk text-grayLight text-lg font-medium"
+      >
+        Select Token
+      </Label>
+      <Input
+        className="rounded-none border border-GrayLight font-medium"
+        placeholder="Amount"
+      />{" "}
+      <div className="mt-5">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              className="flex justify-between w-full h-17 px-3 border border-grayLight-1 rounded-md text-textBlack font-plex-grotesk text-[24px]"
+            >
+              3 months
+              <ChevronDownIcon className="w-4 h-4 ml-2" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            align="start"
+            className="w-full border border-gray-200 rounded-md shadow-md"
+          >
+            <div className="flex flex-col"></div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
+
+function AddToken({
+  tokenImage,
+  tokenName,
+}: {
+  tokenImage: any;
+  tokenName: string;
+}) {
+  return (
+    <div className="border border-solid border-grayLight p-5 relative">
+      <div className="flex flex-col gap-4">
+        <Image src={tokenImage} alt="token" />
+        <span className="text-[24px] text-textBlack">{tokenName}</span>
+      </div>
+      <Button className="bg-black absolute right-0 top-0 h-full">
+        <Image src={add} alt="add" />
+      </Button>
+    </div>
+  );
+}
 function page() {
-  return <div></div>;
+  const tokenList = [
+    {
+      tokenImage: tokenImage,
+      tokenName: "USDc",
+    },
+    {
+      tokenImage: tokenImage,
+      tokenName: "USDc",
+    },
+    {
+      tokenImage: tokenImage,
+      tokenName: "USDc",
+    },
+    {
+      tokenImage: tokenImage,
+      tokenName: "USDc",
+    },
+  ];
+  return (
+    <div>
+      <div className="grid lg:grid-cols-4 grid-cols-1">
+        <div className="col-span-1 flex flex-col p-5 gap-8 border border-solid">
+          {tokenList.map((token) => {
+            return (
+              <AddToken
+                tokenImage={token.tokenImage}
+                tokenName={token.tokenName}
+              />
+            );
+          })}
+        </div>
+        <div className="hidden lg:flex col-span-2  flex-col items-center justify-center border border-solid relative">
+          <Image src={dcdsFrame} alt="dcds" />
+          <span className="absolute left-3 bottom-3 underline font-plex-grotesk text-textBlack font-medium">
+            How it works?
+          </span>
+          <div className="mt-[12px] border border-solid">
+            <span className="font-plex-grotesk text-[12px] bg-[#FFE0E0] text-[#FF0000] px-3 py-2">
+              This fund will be exposed to liquidation risks
+            </span>
+          </div>
+        </div>
+        <div className="col-span-1 border border-solid flex flex-col justify-between">
+          <div className="p-5">
+            <span className="font-plex-grotesk text-textBlack text-[24px] font-medium">
+              Deposit Funds
+            </span>
+            <SelectToken />
+            <div className="py-4 flex">
+              <span className="text-grayLight font-normal font-plex-grotesk text-[18px]">
+                Opt for liquidity gains?
+              </span>
+            </div>
+            <div className="p-3 bg-[#FFF0CA] text-[12px] font-plex-grotesk text-grayLight font-medium">
+              Note: Your amount will be used to offer protection to borrowers &
+              protocol in return for fixed yields
+            </div>
+          </div>
+          <div>
+            <AdditionalDCDSMetrics />
+            <Button className="bg-black text-white text-[24px] font-plex-grotesk min-h-20 w-full">
+              Deposit
+            </Button>
+          </div>
+        </div>
+      </div>
+      <TokenTvlDetails />
+      <TokenTvlDetails />
+      <TokenTvlDetails />
+    </div>
+  );
 }
 
 export default page;
--- frontenddapp2.0/app/page.tsx
+++ frontenddapp2.0/app/page.tsx
@@ -10,152 +10,152 @@ import arrow from "./assets/arrow-right-02.png";
 import PriceComparison from "./components/PriceComparison";
 import { useRouter } from "next/navigation";
 
-function TransferBetweeHoverElement() {
-  return (
-    <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
-      <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
-        Transfer Between
-      </div>
-      <div className="flex">
-        <div className="flex flex-col">
-          <Image src={ModeImage} alt="Price Graph" className="w-full" />
-          <div className="font-plex-grotesk text-grayLight text-[32px] font-normal">
-            Mode
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <Image src={OptimismImage} alt="Price Graph" className="w-full" />
-          <div className="font-plex-grotesk text-grayLight text-[32px] font-normal">
-            Optimism
-          </div>
-        </div>
-      </div>
-      <Button
-        onClick={() => {
-          //router.push("/mintusdalist");
-        }}
-        className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
-      >
-        Bridge
-        <Image src={arrow} width={42} height={42} alt="arrow" />
-      </Button>
-    </div>
-  );
-}
+// function TransferBetweeHoverElement() {
+//   return (
+//     <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
+//       <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
+//         Transfer Between
+//       </div>
+//       <div className="flex">
+//         <div className="flex flex-col">
+//           <Image src={ModeImage} alt="Price Graph" className="w-full" />
+//           <div className="font-plex-grotesk text-grayLight text-[32px] font-normal">
+//             Mode
+//           </div>
+//         </div>
+//         <div className="flex flex-col">
+//           <Image src={OptimismImage} alt="Price Graph" className="w-full" />
+//           <div className="font-plex-grotesk text-grayLight text-[32px] font-normal">
+//             Optimism
+//           </div>
+//         </div>
+//       </div>
+//       <Button
+//         onClick={() => {
+//           //router.push("/mintusdalist");
+//         }}
+//         className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
+//       >
+//         Bridge
+//         <Image src={arrow} width={42} height={42} alt="arrow" />
+//       </Button>
+//     </div>
+//   );
+// }
 
-function DCDSHoverElement() {
-  return (
-    <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
-      <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
-        $1,000 Invested would have become $3,000
-      </div>
-      <Image src={PriceGraph} alt="Price Graph" className="w-full" />
-      <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
-        Get up to 200% APY
-      </div>
-      <Button
-        onClick={() => {
-          //router.push("/mintusdalist");
-        }}
-        className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
-      >
-        Earn
-        <Image src={arrow} width={42} height={42} alt="arrow" />
-      </Button>
-    </div>
-  );
-}
+// function DCDSHoverElement() {
+//   return (
+//     <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
+//       <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
+//         $1,000 Invested would have become $3,000
+//       </div>
+//       <Image src={PriceGraph} alt="Price Graph" className="w-full" />
+//       <div className="font-plex-grotesk text-textBlack text-[38px] font-medium">
+//         Get up to 200% APY
+//       </div>
+//       <Button
+//         onClick={() => {
+//           //router.push("/mintusdalist");
+//         }}
+//         className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
+//       >
+//         Earn
+//         <Image src={arrow} width={42} height={42} alt="arrow" />
+//       </Button>
+//     </div>
+//   );
+// }
 
-function MintUSDAHoverElement() {
-  return (
-    <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
-      <div className="font-plex-grotesk tetx-textBlack text-[38px] font-medium">
-        100% Synthetic LTV
-      </div>
-      <div className="flex justify-between">
-        <span className="font-plex-grotesk font-medium text-lg text-grayLight">
-          80% Stablecoin
-        </span>
-        <span className="font-plex-grotesk font-medium text-lg text-grayLight">
-          20% Downside Protection
-        </span>
-      </div>
-      <div className="text-[32px] text-textBlack font-medium mb-10">
-        Fee Comparison
-      </div>
-      <div className="flex mb-20">
-        {[
-          {
-            orgName: "Autonomint",
-            amount: "$0.02",
-            tag: "Lowest Fee",
-            tagColor: "#06C160",
-            textColor: "white",
-          },
-          {
-            orgName: "Athermint",
-            amount: "$0.02",
-            tag: "Lowest Fee",
-            tagColor: "#FFF7E0",
-            textColor: "#D6A100",
-          },
-          {
-            orgName: "AthermintXYZ",
-            amount: "$0.02",
-            tag: "Lowest Fee",
-            tagColor: "#FEE2E2",
-            textColor: "#AA0001",
-          },
-        ].map((feeCom) => {
-          return (
-            <PriceComparison
-              orgName={feeCom.orgName}
-              tag={feeCom.tag}
-              amount={feeCom.amount}
-              tagColor={feeCom.tagColor}
-              textColor={feeCom.textColor}
-            />
-          );
-        })}
-      </div>
-      <Button
-        onClick={() => {
-          //router.push("/mintusdalist");
-        }}
-        className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
-      >
-        Mint USDa
-        <Image src={arrow} width={42} height={42} alt="arrow" />
-      </Button>
-    </div>
-  );
-}
+// function MintUSDAHoverElement() {
+//   return (
+//     <div className="flex flex-col justify-between h-full bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] p-8 relative">
+//       <div className="font-plex-grotesk tetx-textBlack text-[38px] font-medium">
+//         100% Synthetic LTV
+//       </div>
+//       <div className="flex justify-between">
+//         <span className="font-plex-grotesk font-medium text-lg text-grayLight">
+//           80% Stablecoin
+//         </span>
+//         <span className="font-plex-grotesk font-medium text-lg text-grayLight">
+//           20% Downside Protection
+//         </span>
+//       </div>
+//       <div className="text-[32px] text-textBlack font-medium mb-10">
+//         Fee Comparison
+//       </div>
+//       <div className="flex mb-20">
+//         {[
+//           {
+//             orgName: "Autonomint",
+//             amount: "$0.02",
+//             tag: "Lowest Fee",
+//             tagColor: "#06C160",
+//             textColor: "white",
+//           },
+//           {
+//             orgName: "Athermint",
+//             amount: "$0.02",
+//             tag: "Lowest Fee",
+//             tagColor: "#FFF7E0",
+//             textColor: "#D6A100",
+//           },
+//           {
+//             orgName: "AthermintXYZ",
+//             amount: "$0.02",
+//             tag: "Lowest Fee",
+//             tagColor: "#FEE2E2",
+//             textColor: "#AA0001",
+//           },
+//         ].map((feeCom) => {
+//           return (
+//             <PriceComparison
+//               orgName={feeCom.orgName}
+//               tag={feeCom.tag}
+//               amount={feeCom.amount}
+//               tagColor={feeCom.tagColor}
+//               textColor={feeCom.textColor}
+//             />
+//           );
+//         })}
+//       </div>
+//       <Button
+//         onClick={() => {
+//           //router.push("/mintusdalist");
+//         }}
+//         className="absolute bottom-0 left-0 w-full mt-13 bg-textBlack text-white text-[32px] flex justify-between h-[70px] hover:bg-textBlack"
+//       >
+//         Mint USDa
+//         <Image src={arrow} width={42} height={42} alt="arrow" />
+//       </Button>
+//     </div>
+//   );
+// }
 
 export default function Home() {
-  const items = [
-    { title: "Mint USDA", subtitle: "TVL - $100,000" },
-    { title: "dCDS", subtitle: "TVL - $100,000" },
-    { title: "Bridge", subtitle: "TVL - $100,000" },
-    { title: "Farm Your Luck", subtitle: "Earn Option Fee" },
-    { title: "Redeem ABOND", subtitle: "" },
-    { title: "Buy", subtitle: "" },
-  ];
+  // const items = [
+  //   { title: "Mint USDA", subtitle: "TVL - $100,000" },
+  //   { title: "dCDS", subtitle: "TVL - $100,000" },
+  //   { title: "Bridge", subtitle: "TVL - $100,000" },
+  //   { title: "Farm Your Luck", subtitle: "Earn Option Fee" },
+  //   { title: "Redeem ABOND", subtitle: "" },
+  //   { title: "Buy", subtitle: "" },
+  // ];
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  // const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-  const pairs = [];
-  for (let i = 0; i < items.length; i += 2) {
-    pairs.push(items.slice(i, i + 2));
-  }
+  // const pairs = [];
+  // for (let i = 0; i < items.length; i += 2) {
+  //   pairs.push(items.slice(i, i + 2));
+  // }
 
   return (
     <div className="w-full">
       <div className="w-full md:block mb-6">
         <Image src={boat} alt="crypto-eth" className="w-full object-cover" />
       </div>
-      <div className="-mt-16">
+      {/* <div className="-mt-16">
         {pairs.map((pair, rowIdx) => (
           <div key={rowIdx} className="flex w-full transition-all">
             {pair.map((item, colIdx) => {
@@ -240,7 +240,7 @@ export default function Home() {
             })}
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
