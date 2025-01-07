a550889ad54df86c7141754688bc35b7a452deb0
Hitesh-Gurnani <newidhiteshgurnani@gmail.com>
2025-01-08 00:32:34 +0530

    feat/added-flip-animation-for-farmyourluck

--- frontenddapp2.0/app/farmyourluck/page.tsx
+++ frontenddapp2.0/app/farmyourluck/page.tsx
@@ -1,28 +1,71 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import "../../styles/farmyourluckstyles.css";
 
-function page() {
+function Page() {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndexForReward, setSelectedIndexForReward] = useState(-1);
+  const [isFlipped, setIsFlipped] = useState(Array.from({ length: 9 }).fill(0));
+
+  const handleClick = (index: number) => {
+    setIsFlipped((prev) => {
+      const newFlipped = [...prev];
+      newFlipped[index] = !newFlipped[index];
+      return newFlipped;
+    });
+    setSelectedIndex(index);
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 h-full">
       <div className="grid col-span-1 lg:col-span-4">
         <div className="grid grid-cols-3 gap-6 md:p-8 p-5">
-          {Array.from({ length: 9 }).map(() => {
-            return (
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div
+              key={index}
+              onClick={() => handleClick(index)}
+              className="group aspect-square lg:aspect-auto lg:h-auto lg:w-auto w-full h-full cursor-pointer"
+            >
               <div
-                className="bg-gradient-to-b from-[#E5F3FF] to-[#FFFDE4] border border-grayLight 
-                  aspect-square lg:aspect-auto lg:h-auto lg:w-auto w-full h-full"
-              ></div>
-            );
-          })}
+                className={`card-container ${
+                  isFlipped[index] ? "flipped" : ""
+                }`}
+              >
+                <div className="card-front">Front</div>
+                <div
+                  className={`card-back ${
+                    selectedIndex === index && selectedIndexForReward === index
+                      ? "selected selected-for-reward"
+                      : selectedIndex === index
+                      ? "selected"
+                      : selectedIndexForReward === index
+                      ? "selected-for-reward"
+                      : ""
+                  }`}
+                >
+                  <div
+                    className={`${
+                      selectedIndex === index &&
+                      selectedIndexForReward === index
+                        ? "selected-for-reward-amount"
+                        : "selected-for-reward-amount-hidden"
+                    }`}
+                  >
+                    0.001 ETH
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <div className="grid col-span-1 lg:col-span-2 lg:p-6 border border-solid-grayLight relative">
         <div className="flex flex-col md:justify-between md:max-h-[calc(100%-80px)] border border-solid border-grayLight lg:border-0 p-5 lg:p-0 gap-20 lg:gap-0">
-          <span className="text-grayLight font-medium  lg:text-[32px] text-[24px] lg:text-center">
+          <span className="text-grayLight font-medium lg:text-[32px] text-[24px] lg:text-center">
             Tap a card to view details
           </span>
           <div className="flex flex-col text-left mb-28 lg:mb-0">
-            <div className=" text-textBlack lg:text-3xl text-[20px] font-medium">
+            <div className="text-textBlack lg:text-3xl text-[20px] font-medium">
               How it works?
             </div>
             <ol className="list-decimal list-inside mt-3 text-grayLight">
@@ -40,13 +83,18 @@ function page() {
               </li>
             </ol>
           </div>
-          <Button className="absolute bg-black w-full left-0 bottom-0 text-white h-[90px] font-bold text-[32px]">
+          <button
+            onClick={() => {
+              setSelectedIndexForReward(selectedIndex);
+            }}
+            className="absolute bg-black w-full left-0 bottom-0 text-white h-[90px] font-bold text-[32px]"
+          >
             Pay $5
-          </Button>
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
