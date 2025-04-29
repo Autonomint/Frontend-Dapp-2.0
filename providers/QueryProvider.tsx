c3221179db16930e77ea8c150220b1cee6506401
Kapil Gupta <kapil0506gupta@gmail.com>
2025-04-29 10:56:43 +0530

    refetch code comment

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -8,31 +8,10 @@ export default function QueryProvider({ children }: PropsWithChildren) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            refetchInterval: 8000,
+            // refetchInterval: 8000,
           },
           mutations: {
-            onError(error: any) {
-              // toast.custom(
-              //   (t: any) => {
-              //     return (
-              //       <div>
-              //         <CustomToast
-              //           key={2}
-              //           props={{
-              //             t: toastId.current,
-              //             toastMainColor: "#B43939",
-              //             headline: `Uhh Ohh! ${error.name}`,
-              //             toastClosebuttonHoverColor: "#e66d6d",
-              //             toastClosebuttonColor: "#C25757",
-              //             type: "error",
-              //           }}
-              //         />
-              //       </div>
-              //     );
-              //   },
-              //   { duration: 5000 }
-              // );
-            },
+            onError(error: any) {},
           },
         },
       })
