a330b000200eb936ef6f7af3733fe681ff63b70a
Kapil Gupta <kapil0506gupta@gmail.com>
2025-06-10 14:08:20 +0530

    lockin issue fix

--- design-systems/organisms/dashboard/portfolio/dcds-deposit-table/dcds-position-table-row.tsx
+++ design-systems/organisms/dashboard/portfolio/dcds-deposit-table/dcds-position-table-row.tsx
@@ -101,7 +101,13 @@ const DcdsPositionTableRow = ({
       </td>
 
       <td className="px-5 py-4 2xl:py-6 ">
-        {(Number(position.lockingPeriod) / 86400).toFixed(0)} days
+        {(Number(position.lockingPeriod) /
+          String(position.lockingPeriod).length >
+        8
+          ? Number(position.lockingPeriod) / 86400000
+          : Number(position.lockingPeriod) / 864000
+        ).toFixed(0)}{" "}
+        days
       </td>
 
       <td className={`px-5 py-4 2xl:py-6  md:text-right md:space-x-12`}>
