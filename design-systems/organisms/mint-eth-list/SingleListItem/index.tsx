4af27130942ee517d92876851fc6171bc8c40f51
Kapil Gupta <kapil0506gupta@gmail.com>
2025-06-10 17:22:00 +0530

    fix

--- design-systems/organisms/mint-eth-list/SingleListItem/index.tsx
+++ design-systems/organisms/mint-eth-list/SingleListItem/index.tsx
@@ -43,17 +43,17 @@ function SingleListItem({ item }: { item: any }) {
     {
       label: "Point",
       value: (
-        <div className="flex items-center justify-center flex-col gap-1">
+        <div className="flex items-center justify-center flex-row-reverse md:flex-col  gap-2 md:gap-1">
           {!!item.boaster &&
             calculateRemainingTimeDate(
               toLocalISOString(new Date(item.boasterTime * 1000))
             ).minutes > 0 &&
             item.boaster > 1 && (
-              <div className="badge mt-1 pulsate w-fit  text-[14px] flex justify-center items-center rounded-full border-[2px] border-green-500 font-bold text-green-600 dark:text-green-400 bg-[#22c55e96] px-1 py-[2px]">
+              <div className="badge mt-1 pulsate w-fit  text-nowrap text-[14px] flex justify-center items-center rounded-full border-[2px] border-green-500 font-bold text-green-600 dark:text-green-400 bg-[#22c55e96] px-1 py-[2px]">
                 {item.boaster}x Points
               </div>
             )}
-          <div className="flex items-baseline justify-center flex-row gap-1">
+          <div className="flex items-baseline justify-center  text-nowrap flex-row gap-1">
             <p>{item.pointsToBeGiven}</p>
             <div className="text-base">{`per ${item.minAmount} ETH`}</div>
           </div>
