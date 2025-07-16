2e3e810bb7ecfa914fe2b2fb7b2d84023dad0a28
Kapil Gupta <kapil0506gupta@gmail.com>
2025-07-16 11:46:40 +0530

    update gain ui in cds withdraw pop up

--- design-systems/molecule/popups/WithdrawModal.tsx
+++ design-systems/molecule/popups/WithdrawModal.tsx
@@ -38,13 +38,16 @@ import { BACKEND_API_URL, scanUrls } from "@/utils/urls";
 import { Options } from "@layerzerolabs/lz-v2-utilities";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Info } from "lucide-react";
+import { CornerDownRight, Info } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { useAccount, useWaitForTransactionReceipt } from "wagmi";
 import LoadingBox from "../LoadingBox";
 import ToastNotification from "../toasts/ToastNotification";
 import ToastNotificationError from "../toasts/ToastNotificationError";
+import Image from "next/image";
+import opIconNew from "@/app/assets/op.svg";
+import baseIconNew from "@/app/assets/op-blue.svg";
 
 export function DcdsWithdrawModal({
   position,
@@ -62,6 +65,8 @@ export function DcdsWithdrawModal({
   const { address, chainId } = useAccount();
 
   const [view, setView] = useState<"withdraw" | "rebalance">("withdraw");
+
+  const [showAPYTooltip, setShowAPYTooltip] = useState(false);
 
   const { data: indexPoint, isLoading: isIndexPointLoading } = useQuery({
     queryKey: ["getPointEarned", address, chainId, position.index],
@@ -123,7 +128,7 @@ export function DcdsWithdrawModal({
       tooltipText: "",
     },
     {
-      headline: "APY till now",
+      headline: "Yields till now",
       value: "0%",
       tooltip: false,
       tooltipText: "APY of the index",
@@ -236,12 +241,12 @@ export function DcdsWithdrawModal({
       headline: `${
         Number(NetworkId.BaseSepolia) == chainId ? "AERO" : "OP"
       } Tokens deposited`,
-      value: `${Number(position?.depositedAmounts?.nativeToken).toFixed(
-        2
-      )} ($${(
-        Number(position?.depositedAmounts?.nativeToken) *
-        Number(position?.nativeTokenPriceAtDeposit)
-      ).toFixed(2)})`,
+      value: Number(position?.depositedAmounts?.nativeToken)
+        ? `${Number(position?.depositedAmounts?.nativeToken).toFixed(2)} ($${(
+            Number(position?.depositedAmounts?.nativeToken) *
+            Number(position?.nativeTokenPriceAtDeposit)
+          ).toFixed(2)})`
+        : null,
       tooltip: false,
       tooltipText: "",
       comment: "Will be converted to USDT at 30% price fall",
@@ -251,12 +256,14 @@ export function DcdsWithdrawModal({
       headline: `${
         Number(NetworkId.BaseSepolia) == chainId ? "AERO" : "OP"
       } Adjusted Deposit Value (Discounted by 30%)`,
-      value: `$${(
-        (Number(position?.depositedAmounts?.nativeToken) *
-          Number(position?.nativeTokenPriceAtDeposit) *
-          70) /
-        100
-      ).toFixed(2)}`,
+      value: Number(position?.depositedAmounts?.nativeToken)
+        ? `$${(
+            (Number(position?.depositedAmounts?.nativeToken) *
+              Number(position?.nativeTokenPriceAtDeposit) *
+              70) /
+            100
+          ).toFixed(2)}`
+        : null,
       tooltip: true,
       tooltipText:
         "After factoring in volatility, the token is deposited at 30% lower value",
@@ -266,7 +273,9 @@ export function DcdsWithdrawModal({
       headline: `${
         Number(NetworkId.BaseSepolia) == chainId ? "AERO" : "OP"
       } Token Price at Deposit`,
-      value: Number(position?.nativeTokenPriceAtDeposit).toFixed(4),
+      value: Number(position?.nativeTokenPriceAtDeposit)
+        ? Number(position?.nativeTokenPriceAtDeposit).toFixed(4)
+        : null,
       tooltip: false,
       tooltipText: "",
     },
@@ -368,14 +377,14 @@ export function DcdsWithdrawModal({
       updatedData[0].value =
         position.depositedAmint == "undefined" ||
         position.depositedAmint == "NaN"
-          ? "0"
+          ? 0
           : position.depositedAmounts.usda;
-      // Update depositedAmint value
+      // Update deposited USDA+ value
       updatedData[1].value =
         position.depositedUsdt == "undefined" || position.depositedUsdt == "NaN"
-          ? "0"
+          ? 0
           : position.depositedAmounts.usdt;
-      // Update depositedAmint value
+      // Update deposited ETH value
       updatedData[2].value = `$${Number(position.ethPriceAtDeposit) / 100}`;
       // Update points earned till now
       updatedData[3].value = `${Math.floor(indexPoint?.[1]) || "0"}`;
@@ -672,6 +681,8 @@ export function DcdsWithdrawModal({
   // fetching layer zero transaction data to add loading state to user to initiate transaction
   const { readyForNewTx } = useLayerZeroMessages();
 
+  console.log(depositData, "depositData");
+
   return (
     <Dialog open={isDialogOpen} onOpenChange={handleCloseDialog}>
       <DialogContent className="max-w-[98%] sm:max-w-[610px] bg-white dark:border-[1px] dark:border-grayLight  dark:bg-[#0D0D0D] ">
@@ -736,7 +747,7 @@ export function DcdsWithdrawModal({
             <div>
               <div className="h-[275px] overflow-auto no-scrollbar">
                 {NewDetails.map((dcdsWidthDrawMetricsObj, idx) => {
-                  return (
+                  return dcdsWidthDrawMetricsObj.value ? (
                     <div
                       key={idx}
                       className="flex flex-col justify-between mb-2"
@@ -774,86 +785,146 @@ export function DcdsWithdrawModal({
                         </div>
                       )}
                     </div>
-                  );
+                  ) : null;
                 })}
-                {depositData.map((dcdsWidthDrawMetricsObj, idx) => {
-                  return (
-                    <div key={idx} className="flex justify-between mb-2">
-                      <span
-                        className={`text-[16px] md:text-[18px] flex items-center  font-medium text-grayLight ${
-                          dcdsWidthDrawMetricsObj?.titleColor || ""
-                        }`}
+                {depositData
+                  .filter((depositData) => {
+                    if (
+                      ["USDA+ Deposited", "USDT Deposited"].includes(
+                        depositData.headline
+                      ) &&
+                      Number(depositData.value) <= 0
+                    ) {
+                      return false;
+                    }
+                    return true;
+                  })
+                  .map((dcdsWidthDrawMetricsObj, idx) => {
+                    return (
+                      <div
+                        key={idx}
+                        className="flex relative justify-between mb-2"
                       >
-                        {" "}
-                        {dcdsWidthDrawMetricsObj.headline}
-                        {dcdsWidthDrawMetricsObj.tooltip && (
-                          <Tooltip delayDuration={100}>
-                            <TooltipTrigger asChild>
-                              <Info width={18} height={18} className="ml-2" />
-                            </TooltipTrigger>
-                            <TooltipContent className="bg-white dark:bg-black">
-                              <p>{dcdsWidthDrawMetricsObj.tooltipText}</p>
-                            </TooltipContent>
-                          </Tooltip>
-                        )}
-                      </span>
-                      <span
-                        className={`text-[16px] md:text-[18px] dark:text-white font-medium text-textBlack ${
-                          dcdsWidthDrawMetricsObj?.valueColor || ""
-                        }`}
-                      >
-                        {dcdsWidthDrawMetricsObj.value}
-                      </span>
-                    </div>
-                  );
-                })}
+                        <span
+                          className={`text-[16px] md:text-[18px] flex items-center  font-medium text-grayLight ${
+                            dcdsWidthDrawMetricsObj?.titleColor || ""
+                          }`}
+                        >
+                          {" "}
+                          {dcdsWidthDrawMetricsObj.headline}
+                          {dcdsWidthDrawMetricsObj.tooltip && (
+                            <Tooltip delayDuration={100}>
+                              <TooltipTrigger asChild>
+                                <Info width={18} height={18} className="ml-2" />
+                              </TooltipTrigger>
+                              <TooltipContent className="bg-white dark:bg-black">
+                                <p>{dcdsWidthDrawMetricsObj.tooltipText}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          )}
+                        </span>
+                        <span
+                          className={`text-[16px] md:text-[18px] dark:text-white font-medium text-textBlack ${
+                            dcdsWidthDrawMetricsObj?.valueColor || ""
+                          } `}
+                        >
+                          {dcdsWidthDrawMetricsObj.value}
+                        </span>
+                      </div>
+                    );
+                  })}
               </div>
-              <div className="flex w-full pt-4">
-                <div className="flex-1 flex flex-col justify-start items-start  gap-  border border-solid border-grayLight py-2 px-4">
-                  <Label className=" tex-[16px] md:text-[16px]  font-normal text-[#777777]">
-                    Option Fee + Liquidation Gains
-                  </Label>
-                  <Label className=" text-[20px] md:text-[24px] font-medium dark:text-white">
-                    {Number(
-                      apy == undefined
-                        ? 0
-                        : position.status !== "DEPOSITED"
-                        ? position?.apys?.amountAccured
-                        : apy[1]
-                    ).toFixed(4)}
-                  </Label>
-                  <div className="h-2"></div>
-                  <Label className=" text-[12px] font-normal text-[#777777]">
-                    Price Gains
-                  </Label>
-                  <Label className=" text-[14px] font-medium dark:text-white">
-                    {Number(
-                      apy == undefined
-                        ? 0
-                        : position.status !== "DEPOSITED"
-                        ? position?.apys?.priceChangePL
-                        : apy[2]
-                    ).toFixed(4)}
-                  </Label>
+              <div className="flex w-full mt-4  border-solid  border-[1px]  justify-between border-gray-200 rounded-[12px] bg-gray-100 dark:border-[rgb(51,51,51)] dark:bg-[#121212] flex-col sm:flex-row">
+                <div className="flex-1 relative flex flex-col justify-start items-start  gap- border-r-0    py-2 px-4">
+                  <div className="flex flex-col w-full  items-start justify-between">
+                    <Label className=" text-[22px] font-bold  md:text-[26px] text-green-600 dark:text-green-500  ">
+                      $
+                      {Number(
+                        apy == undefined
+                          ? 0
+                          : position.status !== "DEPOSITED"
+                          ? position?.apys?.amountAccured
+                          : apy[1]
+                      ).toFixed(4)}
+                    </Label>
+                    <div>
+                      <Label className="text-[14px] font-normal text-[#777777]">
+                        Option Fee + Liquidation Gains
+                      </Label>
+                    </div>
+                  </div>
+
+                  <div className="flex w-full relative items-center justify-between">
+                    <CornerDownRight className="absolute left-0 top-[-1px] stroke-black dark:stroke-white" />
+                    <div className="flex gap-1 ml-8">
+                      <Label className=" text-[14px] font-normal text-[#777777]">
+                        Fixed Yields
+                      </Label>
+                      <div className="flex gap-1">
+                        <Image
+                          src={baseIconNew}
+                          alt="eth"
+                          width={18}
+                          height={18}
+                        />
+                        <Image
+                          src={opIconNew}
+                          alt="eth"
+                          width={18}
+                          height={18}
+                        />
+                      </div>
+                    </div>
+                    <Label className="text-[18px] md:text-[20px] font-medium dark:text-white">
+                      {`${Number(
+                        apy == undefined
+                          ? 0
+                          : position.status !== "DEPOSITED"
+                          ? position?.apys?.currentTimeAPYTillNow
+                          : apy[5]
+                      ).toFixed(2)}%`}
+                    </Label>
+                  </div>
                 </div>
-                <div className="flex-1 w-full flex flex-col justify-center items-start  gap-1 border border-solid border-grayLight py-2 px-4 font-medium">
-                  <Label className="text-[14px] md:text-[18px] font-normal text-[#777777]">
-                    Yield Earned (All chains)
-                  </Label>
-                  <Label className="text-[20px] md:text-[24px] font-medium dark:text-white">
-                    {`${Number(
-                      apy == undefined
-                        ? 0
-                        : position.status !== "DEPOSITED"
-                        ? position?.apys?.currentTimeAPYTillNow
-                        : apy[5]
-                    ).toFixed(2)}%`}
-                  </Label>
+                <div className="flex-1 w-full flex flex-col justify-center items-start  gap-1  py-2 px-4 font-medium">
+                  <div className="flex flex-col w-full items-start justify-between">
+                    <Label className="text-[22px] md:text-[26px] font-medium dark:text-white">
+                      $
+                      {Number(
+                        apy == undefined
+                          ? 0
+                          : position.status !== "DEPOSITED"
+                          ? position?.apys?.priceChangePL
+                          : apy[2]
+                      ).toFixed(4)}
+                    </Label>
+                    <Label className="text-[14px]  font-normal text-[#777777]">
+                      Price Gains
+                    </Label>
+                  </div>
+                  <div className="flex w-full relative  items-center justify-between">
+                    <CornerDownRight className="absolute left-0 top-[-1px] stroke-black dark:stroke-white" />
+                    <Label className="text-[14px] font-normal text-[#777777] ml-8">
+                      Variable yields
+                    </Label>
+                    <Label className="text-[18px] md:text-[20px] font-medium dark:text-white">
+                      {Number(
+                        apy == undefined
+                          ? 0
+                          : position.status !== "DEPOSITED"
+                          ? Number(position?.apys?.priceChangePL) /
+                            Number(position?.totalDepositedAmount)
+                          : Number(apy[2]) /
+                            Number(position?.totalDepositedAmount)
+                      ).toFixed(2)}
+                      %
+                    </Label>
+                  </div>
                 </div>
               </div>
               <Typography
                 variant="regular"
-                className="text-[14px] md:text-[16px] my-3 text-[#777777] "
+                className="text-[14px] my-3 text-[#777777] "
               >
                 Note: Your amount will be used to offer protection to borrowers
                 & protocol in return for fixed yields
