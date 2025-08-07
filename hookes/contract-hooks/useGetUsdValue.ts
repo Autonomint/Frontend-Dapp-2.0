ee3eb1f5ccdc0d010cc95c91a1af77186cf57aa4
Kapil Gupta <kapil0506gupta@gmail.com>
2025-08-07 19:05:11 +0530

    build fix

--- hookes/contract-hooks/useGetUsdValue.ts
+++ hookes/contract-hooks/useGetUsdValue.ts
@@ -42,8 +42,12 @@ const useGetUsdValue = (assetAddress?: ValueOf<typeof borrowAssetsAddress>) => {
           ], // Default to ETH address if assetAddress is not provided
     ],
     query: { enabled: !!address && !!chainId }, // Only run the query if wallet is connected and chain is available
-  });
-
+  }) as {
+    data: bigint[] | undefined;
+    error: Error | undefined;
+    isSuccess: boolean;
+    isPending: boolean;
+  };
 
   return {
     isUsdValuePending, // Indicates if the USD value query is still loading
