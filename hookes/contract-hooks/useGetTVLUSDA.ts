99dc0aeb4a41a06bdbc66aff43adf6c0cfb65fd2
Kapil Gupta <kapil0506gupta@gmail.com>
2025-08-26 18:23:15 +0530

    fix TVL in cds page

--- hookes/contract-hooks/useGetTVLUSDA.ts
+++ hookes/contract-hooks/useGetTVLUSDA.ts
@@ -34,20 +34,19 @@ const useGetTVLUSDA = (tokenAddress: `0x${string}`) => {
   });
 
   const provider = new ethers.JsonRpcProvider(
-    `${
-      rpcUrls[
-        chainId === NetworkId.BaseSepolia
-          ? NetworkId.Optimism
-          : NetworkId.BaseSepolia
-      ]
+    `${rpcUrls[
+    chainId === NetworkId.BaseSepolia
+      ? NetworkId.Optimism
+      : NetworkId.BaseSepolia
+    ]
     }/${alchemyApiKeys}`
   );
 
   const cdsContract = new ethers.Contract(
     cdsAddress[
-      chainId === NetworkId.BaseSepolia
-        ? (NetworkId.Optimism as keyof typeof cdsAddress)
-        : (NetworkId.BaseSepolia as keyof typeof cdsAddress)
+    chainId === NetworkId.BaseSepolia
+      ? (NetworkId.Optimism as keyof typeof cdsAddress)
+      : (NetworkId.BaseSepolia as keyof typeof cdsAddress)
     ],
     cdsAbi,
     provider
@@ -67,7 +66,7 @@ const useGetTVLUSDA = (tokenAddress: `0x${string}`) => {
     setOtherChainUSDa(usdaTvl2);
   };
 
- 
+
 
   return {
     isTVLPending,
@@ -102,20 +101,19 @@ const useGetTVLBothChain = (tokenAddressArr: `0x${string}`[]) => {
 
   // Provider for fetching other chain data
   const provider = new ethers.JsonRpcProvider(
-    `${
-      rpcUrls[
-        chainId === NetworkId.BaseSepolia
-          ? NetworkId.Optimism
-          : NetworkId.BaseSepolia
-      ]
+    `${rpcUrls[
+    chainId === NetworkId.BaseSepolia
+      ? NetworkId.Optimism
+      : NetworkId.BaseSepolia
+    ]
     }/${alchemyApiKeys}`
   );
 
   const cdsContract = new ethers.Contract(
     cdsAddress[
-      chainId === NetworkId.BaseSepolia
-        ? NetworkId.Optimism
-        : (NetworkId.BaseSepolia as keyof typeof cdsAddress)
+    chainId === NetworkId.BaseSepolia
+      ? NetworkId.Optimism
+      : (NetworkId.BaseSepolia as keyof typeof cdsAddress)
     ],
     cdsAbi,
     provider
@@ -134,10 +132,7 @@ const useGetTVLBothChain = (tokenAddressArr: `0x${string}`[]) => {
     const tvls = [];
     // fetching the tvl of the tokens on the other chain
     // removing OP address by slicing because its values already from both chain
-    for (const tokenAddress of otherChainAddressList.slice(
-      0,
-      otherChainAddressList.length - 1
-    )) {
+    for (const tokenAddress of otherChainAddressList) {
       const tvl = await cdsContract.getTokenDepositedTillNow(tokenAddress);
       tvls.push(tvl);
     }
@@ -146,15 +141,14 @@ const useGetTVLBothChain = (tokenAddressArr: `0x${string}`[]) => {
     setOtherChainDataLoading(false);
   };
 
+
   // calculating the total tvl of the tokens on both chains
   const totalTVLList = useMemo(() => {
     return tvlValue?.map(
       (item: any, index: number) =>
-        Number(item) + Number(otherChainTvl[index] || 0)
+        Number(item) + (index === 2 ? 0 : Number(otherChainTvl[index] || 0))
     );
   }, [tvlValue, otherChainTvl]);
-
-
 
   return {
     isTVLPending: isTVLPending || otherChainDataLoading,
