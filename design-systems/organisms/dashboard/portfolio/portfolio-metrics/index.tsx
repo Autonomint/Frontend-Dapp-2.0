edebf7df39310b9b9c8e9ab72441f69d04fe4221
Kapil Gupta <kapil0506gupta@gmail.com>
2025-10-06 16:20:52 +0530

    og tag ui fix

--- design-systems/organisms/dashboard/portfolio/portfolio-metrics/index.tsx
+++ design-systems/organisms/dashboard/portfolio/portfolio-metrics/index.tsx
@@ -51,9 +51,9 @@ function PortfolioMetrics({
             {subHeading === "Points (All Chain)" && isOG && (
               <div className="flex relative  items-center w-fit   ml-[-px]  ">
                 <span className="absolute  z-10 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ABFFDE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-star-icon lucide-circle-star"><path d="M11.051 7.616a1 1 0 0 1 1.909.024l.737 1.452a1 1 0 0 0 .737.535l1.634.256a1 1 0 0 1 .588 1.806l-1.172 1.168a1 1 0 0 0-.282.866l.259 1.613a1 1 0 0 1-1.541 1.134l-1.465-.75a1 1 0 0 0-.912 0l-1.465.75a1 1 0 0 1-1.539-1.133l.258-1.613a1 1 0 0 0-.282-.867l-1.156-1.152a1 1 0 0 1 .572-1.822l1.633-.256a1 1 0 0 0 .737-.535z" /><circle cx="12" cy="12" r="10" /></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-star-icon lucide-circle-star stroke-[#3cc68f] dark:stroke-[#ABFFDE]"><path d="M11.051 7.616a1 1 0 0 1 1.909.024l.737 1.452a1 1 0 0 0 .737.535l1.634.256a1 1 0 0 1 .588 1.806l-1.172 1.168a1 1 0 0 0-.282.866l.259 1.613a1 1 0 0 1-1.541 1.134l-1.465-.75a1 1 0 0 0-.912 0l-1.465.75a1 1 0 0 1-1.539-1.133l.258-1.613a1 1 0 0 0-.282-.867l-1.156-1.152a1 1 0 0 1 .572-1.822l1.633-.256a1 1 0 0 0 .737-.535z" /><circle cx="12" cy="12" r="10" /></svg>
                 </span>
-                <span className="text-black dark:text-white rounded-[24px] pl-6 pr-2 py-[1px] text-[14px] border-[1px] border-grayLight mr-2">
+                <span className="text-black dark:text-white rounded-[24px] pl-6 pr-2 py-0 text-[14px] border-[1px] border-grayLight mr-2">
                   OG
                 </span>
               </div>
