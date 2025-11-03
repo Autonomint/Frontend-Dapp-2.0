9fdfa106de6c4bcf6d4d43a76bd422796474368a
Kapil Gupta <kapil0506gupta@gmail.com>
2025-11-03 20:19:15 +0530

    fix issue bold cbbtc

--- design-systems/templates/dcds/page.tsx
+++ design-systems/templates/dcds/page.tsx
@@ -438,12 +438,19 @@ function DCDSTemplate() {
         ));
         return;
       }
+      // filtering the token list based on the hedge asset
+      // Removing bold token from the token list if cbBTC is selected
+      const filteredTokenList = tokenList.filter(
+        (token) =>
+          formik.values.hedgeAsset === "BOLD" && token.tokenName === "BOLD"
+      );
+
       handleDcdsDeposit?.(
         [
           {
             user: address as `0x${string}`,
             // token addresses
-            tokenAddresses: tokenList.map((token) => {
+            tokenAddresses: filteredTokenList.map((token) => {
               const tokenDetail = selectedTokens.find((selectedToken) => {
                 return selectedToken.tokenAddress === token.tokenAddress;
               });
@@ -451,7 +458,7 @@ function DCDSTemplate() {
                 zeroAddress) as `0x${string}`;
             }),
             // token amount in wei
-            tokenAmounts: tokenList.map((token) => {
+            tokenAmounts: filteredTokenList.map((token) => {
               const tokenDetail = selectedTokens.find((selectedToken) => {
                 return selectedToken.tokenAddress === token.tokenAddress;
               });
@@ -1320,6 +1327,8 @@ function DCDSTemplate() {
     return list;
   }, [tokenList]);
   console.log("formik.values", formik);
+  console.log(tokenList, "tokenList");
+
   return (
     <div>
       <AppNavbar activeBack={showBack} />
