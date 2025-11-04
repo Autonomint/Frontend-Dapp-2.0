0cb42ed1836bf1652a8de17e6f181a1b12aebc8d
Kapil Gupta <kapil0506gupta@gmail.com>
2025-11-04 12:52:15 +0530

    fix refresh cbBTC

--- design-systems/templates/dashboard/portfolio/index.tsx
+++ design-systems/templates/dashboard/portfolio/index.tsx
@@ -115,9 +115,9 @@ function PortfolioTemplate() {
   }, [portfolioTab]);
 
   // refresh borrowed table data for backend data refetch from blockchain
-  const RefreshTableData = async () => {
+  const RefreshTableCBBTCData = async () => {
     const res = await fetch(
-      `${BACKEND_API_URL}/borrows/refresh/${chainId}/${address}`,
+      `${BACKEND_API_URL}/refresh/${chainId}/${address}/cbBTC`,
       {
         method: "POST",
       }
@@ -126,9 +126,9 @@ function PortfolioTemplate() {
   };
 
   // refresh cds table data for backend data refetch from blockchain
-  const RefreshTableDataCds = async () => {
+  const RefreshTableETHData = async () => {
     const res = await fetch(
-      `${BACKEND_API_URL}/cds/refresh/${chainId}/${address}`,
+      `${BACKEND_API_URL}/cds/refresh/${chainId}/${address}/ETH`,
       {
         method: "POST",
       }
@@ -141,13 +141,18 @@ function PortfolioTemplate() {
     try {
       if (!address || !chainId || !isConnected) return;
       setRefreshLoading(true);
+      // Calling api base on select tab so that we can update select tab first
       if (tabPosition == "Borrowed") {
-        await RefreshTableData();
+        await RefreshTableCBBTCData();
+        await RefreshTableETHData();
         await positionListRefetch();
+        await dcdsPositionListRefetch();
       }
       if (tabPosition == "Deposited") {
-        await RefreshTableDataCds();
+        await RefreshTableETHData();
+        await RefreshTableCBBTCData();
         await dcdsPositionListRefetch();
+        await positionListRefetch();
       }
     } catch (error) {
     } finally {
