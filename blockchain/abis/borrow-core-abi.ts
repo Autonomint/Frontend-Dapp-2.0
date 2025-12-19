26a311694f8c22ee8bc19166043f2c191bcb43cd
Kapil Gupta <kapil0506gupta@gmail.com>
2025-12-19 16:52:35 +0530

    abi updated

--- blockchain/abis/borrow-core-abi.ts
+++ blockchain/abis/borrow-core-abi.ts
@@ -119,6 +119,17 @@ export const borowCoreABI = [
     "type": "error"
   },
   {
+    "inputs": [
+      {
+        "internalType": "uint128",
+        "name": "validity",
+        "type": "uint128"
+      }
+    ],
+    "name": "Borrow_Invalid_Validity",
+    "type": "error"
+  },
+  {
     "inputs": [],
     "name": "Borrow_LTVIsZero",
     "type": "error"
@@ -400,6 +411,12 @@ export const borowCoreABI = [
         "internalType": "uint256",
         "name": "aBondCr",
         "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint128",
+        "name": "hedgeValidity",
+        "type": "uint128"
       }
     ],
     "name": "Deposit",
@@ -492,8 +509,20 @@ export const borowCoreABI = [
       {
         "indexed": false,
         "internalType": "uint256",
+        "name": "optionsFees",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
         "name": "timestamp",
         "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint128",
+        "name": "hedgeValidity",
+        "type": "uint128"
       }
     ],
     "name": "Renewed",
@@ -709,6 +738,11 @@ export const borowCoreABI = [
             "internalType": "int128",
             "name": "plFromExpired",
             "type": "int128"
+          },
+          {
+            "internalType": "uint128",
+            "name": "hedgeValidity",
+            "type": "uint128"
           }
         ],
         "internalType": "struct IBorrowingCore.BorrowDepositParams",
@@ -944,6 +978,16 @@ export const borowCoreABI = [
         "internalType": "uint64",
         "name": "index",
         "type": "uint64"
+      },
+      {
+        "internalType": "uint128",
+        "name": "hedgeValidity",
+        "type": "uint128"
+      },
+      {
+        "internalType": "uint256",
+        "name": "volatility",
+        "type": "uint256"
       }
     ],
     "name": "getOptionFeesToPay",
@@ -1113,6 +1157,38 @@ export const borowCoreABI = [
         "internalType": "uint64",
         "name": "index",
         "type": "uint64"
+      },
+      {
+        "internalType": "uint128",
+        "name": "hedgeValidity",
+        "type": "uint128"
+      },
+      {
+        "internalType": "uint256",
+        "name": "volatility",
+        "type": "uint256"
+      },
+      {
+        "components": [
+          {
+            "internalType": "uint256",
+            "name": "nonce",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "deadline",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bytes",
+            "name": "signature",
+            "type": "bytes"
+          }
+        ],
+        "internalType": "struct IBorrowingCore.EIP712VerifyParams",
+        "name": "verifyParams",
+        "type": "tuple"
       }
     ],
     "name": "renewOptions",
