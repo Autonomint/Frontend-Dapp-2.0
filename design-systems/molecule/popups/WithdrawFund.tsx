e630522355f172c21996ef2fc71352402c516818
Kapil Gupta <kapil0506gupta@gmail.com>
2026-01-05 14:03:59 +0530

    fix upside krwq borrow

--- design-systems/molecule/popups/WithdrawFund.tsx
+++ design-systems/molecule/popups/WithdrawFund.tsx
@@ -5,6 +5,7 @@ import {
   borrowCoreAddress,
   borrowingContractAddress,
   borrowWithdrawCoreAddress,
+  optionContractAddress,
   testusdtAbiAddress,
   usDaAddress,
 } from "@/blockchain/contracts";
@@ -72,6 +73,7 @@ import useGetLtv from "@/hookes/contract-hooks/useGetLtv";
 import { GenericDropdownMenu } from "@/design-systems/atoms/DropdownCustom/GenericDropdownMenu";
 import useGetBorrowRenewSignedData from "@/hookes/api-hooks/useGetBorrowRenewSignedData";
 import { InfoIcon } from "lucide-react";
+import { optionABI } from "@/blockchain/abis/option";
 export function WithdrawFund({
   position,
   isDialogOpen,
@@ -336,7 +338,7 @@ export function WithdrawFund({
       ? borrowCoreAddress
       : borrowingContractAddress;
   // getting token details
-  const { data: assetDetails, refetch: refetchCurrentData } = useReadContract({
+  const { data: assetDetails } = useReadContract({
     abi: borrowingContractAbi,
     address: contract[
       chainId as keyof typeof borrowingContractAddress
@@ -385,6 +387,23 @@ export function WithdrawFund({
       ],
     ],
   }) as { data: number | undefined; isLoading: boolean; refetch: () => void };
+
+  // Custom hook to fetch the current strike price percent limit
+  const { data: currentStrikePricePercentLimit, refetch: refetchCurrentData } =
+    useReadContract({
+      abi: optionABI,
+      address:
+        optionContractAddress[chainId as keyof typeof optionContractAddress],
+      functionName: "strikePricePercentLimits",
+      args: [
+        BorrowAssetsEnum[
+          position.collateralType as keyof typeof BorrowAssetsEnum
+        ],
+      ],
+      query: {
+        select: (data) => Number(data || 0) / 100,
+      },
+    });
 
   // fetching allowance of usda for repay
   const {
@@ -484,7 +503,9 @@ export function WithdrawFund({
 
       // calculate upside at deposit time
       const upsideAt =
-        (Number(position.depositedAmountInETH) * Number(ethPriceAtDep) * 5) /
+        (Number(position.depositedAmountInETH) *
+          Number(ethPriceAtDep) *
+          (currentStrikePricePercentLimit || 0)) /
         100;
 
       // calculate price difference
@@ -940,7 +961,6 @@ export function WithdrawFund({
         (usdaHashData && usdaHashSucces) ||
         (usdtHashData && usdtHashSucces)
       ) {
-
         if (toggleView == "repay") {
           setIsApproveLoadingLocal(false);
           setTimeout(() => {
