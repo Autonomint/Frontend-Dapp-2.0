8d3cba5ab1c37e4f5dcda1172c86dda34fd62e49
Kapil Gupta <kapil0506gupta@gmail.com>
2026-03-06 17:11:13 +0530

    fix option fee

--- design-systems/organisms/mint-page/input-form/index.tsx
+++ design-systems/organisms/mint-page/input-form/index.tsx
@@ -133,6 +133,7 @@ function InputForm({ currency }: { currency: string }) {
         : (currency as keyof typeof borrowAssetsAddress)
     ],
     currency.toLocaleLowerCase() === "krwq",
+    currency.toLocaleLowerCase() === "eurc",
   );
 
   // Selected asset price
@@ -426,7 +427,9 @@ function InputForm({ currency }: { currency: string }) {
     String(formik.values.collateralAmount),
     (currency === "KRWQ"
       ? parseUnits(String(assetPrice), 8)
-      : assetPrice || 0) as number,
+      : currency === "EURC"
+        ? parseUnits(String(assetPrice), 6)
+        : assetPrice || 0) as number,
     formik.values.strikePricePercent,
     currency === "cbBTC"
       ? "BTC"
@@ -437,6 +440,7 @@ function InputForm({ currency }: { currency: string }) {
           : "ETH",
     Number(formik.values.hedgeDuration),
   );
+  console.log(assetPrice, "assetPrice");
   // Custom hook to fetch the current strike price percent limit
   const { data: currentStrikePricePercentLimit } = useReadContract({
     abi: optionABI,
@@ -814,7 +818,7 @@ function InputForm({ currency }: { currency: string }) {
                   <span className="text-grayLight">{currency} Price: </span>{" "}
                   <span className=" dark:text-white text-textBlack font-semibold ml-1">
                     $
-                    {currency === "KRWQ"
+                    {currency === "KRWQ" || currency === "EURC"
                       ? Number(selectedAssetPrice)
                       : Number(selectedAssetPrice) / 100 || 0}
                   </span>
