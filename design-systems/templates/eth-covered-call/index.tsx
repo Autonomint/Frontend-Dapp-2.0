c6ceecd7ba0b2ffcb61e4bc1a897336b51680d49
Kapil Gupta <kapil0506gupta@gmail.com>
2026-08-05 13:44:40 +0530

    fix: hide first strike price option for ETH

--- design-systems/templates/eth-covered-call/index.tsx
+++ design-systems/templates/eth-covered-call/index.tsx
@@ -176,7 +176,8 @@ const CoveredCallTemplate = ({
   });
 
   const MAX_STRIKE_PRICE_OPTIONS = 6;
-  const displayPriceOptions = priceOptions.slice(0, MAX_STRIKE_PRICE_OPTIONS);
+  const filteredPriceOptions = selectedTicker === "ETH" ? priceOptions.slice(1) : priceOptions;
+  const displayPriceOptions = filteredPriceOptions.slice(0, MAX_STRIKE_PRICE_OPTIONS);
 
   const handlePriceSelection = (priceOption: PriceOption) => {
     setSelectedPrice(priceOption);
